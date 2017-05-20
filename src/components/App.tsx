e2908c25b28bca0d9e5fe2b8e5b3cbdbcdca3ecb
betchi <shinichi.minobe@gmail.com>
2017-05-21 01:11:50 +0900

    express

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,29 @@ import * as React from 'react';
 interface Props {
 }
 
-class App extends React.Component<Props, {}> {
+interface State {
+  count: number;
+}
+
+class App extends React.Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
   render(): JSX.Element {
     return (
       <div>
-        Hello SSR!!!
+        <p>Count: {this.state.count}</p>
+        <button onClick={e => this.increment()}>Increment</button>
       </div>
     );
   }
--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,18 +1,35 @@
-// let express = require('express');
-// let React = require('react');
-// let ReactDOMServer = require('react-dom/server');
-// let App = require('../components/App');
+import * as express from 'express';
+import * as React from 'react';
+import * as DOMServer from 'react-dom/server';
+import App from '../components/App';
 
-// const app = express();
+const app = express();
 
-// app.get('/', (req, res) => {
-//   res.send(
-//     ReactDOMServer.renderToString(
-//       <App />
-//     )
-//   );
-// });
+app.use(express.static('public'));
 
-// app.listen(3000, () => {
-//   console.log('Example app listening on port 3000!');
-// });
+function renderFullPage(renderedContent: string) {
+  return `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>React Starter Kit</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" type="text/css" media="all">
+</head>
+<body>
+  <div id="content">${renderedContent}</div>
+  <script type="text/javascript" charset="utf-8" src="bundle.js"></script>
+</body>
+</html>`;
+}
+
+app.get('/abc', (req, res) => {
+  const renderedContent = DOMServer.renderToString(<App />);
+  const renderedPage = renderFullPage(renderedContent);
+  res.status(200).send(renderedPage);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log('Listening on port ' + port + '!');
+});
