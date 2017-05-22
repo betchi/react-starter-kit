399f4a5bb58601e7a41f5f7d95397ae4844024df
betchi <shinichi.minobe@gmail.com>
2017-05-22 13:26:56 +0900

    redux-logger

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,11 +1,19 @@
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
 import * as CounterAction from '../actions/counter';
 import counter from '../reducers/counter';
-import {createStore, combineReducers, Action} from 'redux';
+import {createStore, combineReducers, applyMiddleware, Action, Middleware} from 'redux';
+
+const middleware: Middleware[] = [ thunk ];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
 
 export default createStore(
   combineReducers({
     counter
-  })
+  }),
+  applyMiddleware(...middleware)
 );
 
 export type ReduxState = {
